2124361c7455b4f30e8feea1a7da7b7280ad371e
Tlalok <irbis1307@yandex.ru>
2016-03-01 09:01:06 +0300

    add LsbMethod

---
+++ CandPCI_6/BitmapHelper.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandPCI_6
+{
+    public class BitmapHelper
+    {
+        /// <summary>
+        /// Функция предназначена для извлечения из экземпляра класса Bitmap данных о
+        /// яркости отдельных пикселов и преобразования их в формат byte[,,].
+        /// При этом первый индекс соответствует одной из трех цветовых компонент (R, 
+        /// G или B соответственно), второй - номер строки (координата Y), третий -
+        /// номер столбца (координата X).
+        /// </summary>
+        /// <param name="bmp">Экземпляр Bitmap, из которого необходимо извлечь 
+        /// яркостные данные.</param>
+        /// <returns>Байтовый массив с данными о яркости каждой компоненты
+        /// каждого пиксела.</returns>
+        public unsafe static byte[, ,] BitmapToByteRgbQ(Bitmap bmp)
+        {
+            int width  = bmp.Width,
+                height = bmp.Height;
+            var result = new byte[3, height, width];
+            var bd = bmp.LockBits(new Rectangle(0, 0, width, height), 
+                ImageLockMode.ReadOnly,
+                PixelFormat.Format24bppRgb);
+            try
+            {
+                byte* _curpos;
+                fixed (byte* _result = result)
+                {
+                    byte* _red   = _result, 
+                          _green = _result + 1, 
+                          _blue  = _result + 2;
+                    for (var h = 0; h < height; h++)
+                    {
+                        _curpos = ((byte*)bd.Scan0) + h * bd.Stride;
+                        for (var w = 0; w < width; w++)
+                        {
+                            *_blue = *(_curpos++); 
+                             _blue += 3;
+
+                            *_green = *(_curpos++); 
+                             _green += 3;
+
+                            *_red = *(_curpos++); 
+                             _red += 3;
+                        } 
+                    }
+                }
+            }
+            finally
+            {
+                bmp.UnlockBits(bd);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Функция предназначена для извлечения из экземпляра класса Bitmap данных о
+        /// яркости отдельных пикселов и преобразования их в формат byte[,,].
+        /// При этом третий индекс соответствует одной из трех цветовых компонент (R, 
+        /// G или B соответственно), второй - номер строки (координата Y), первый -
+        /// номер столбца (координата X).
+        /// </summary>
+        /// <param name="bmp">Экземпляр Bitmap, из которого необходимо извлечь 
+        /// яркостные данные.</param>
+        /// <returns>Байтовый массив с данными о яркости каждой компоненты
+        /// каждого пиксела.</returns>
+        public unsafe static byte[, ,] BitmapToByteRgb(Bitmap bmp)
+        {
+            int width = bmp.Width,
+                height = bmp.Height;
+            //byte[, ,] result = new byte[3, height, width];
+            var result = new byte[height, width, 3];
+            var bd = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly,
+                PixelFormat.Format24bppRgb);
+            try
+            {
+                byte* curpos;
+                for (var h = 0; h < height; h++)
+                {
+                    curpos = ((byte*)bd.Scan0) + h * bd.Stride;
+                    for (var w = 0; w < width; w++)
+                    {
+                        result[h, w, 2] = *(curpos++);
+                        result[h, w, 1] = *(curpos++);
+                        result[h, w, 0] = *(curpos++);
+                    }
+                }
+            }
+            finally
+            {
+                bmp.UnlockBits(bd);
+            }
+            return result;
+        }
+
+        public unsafe static byte[] BitmapToByteRgbMarshal(Bitmap bmp)
+        {
+            int width = bmp.Width,
+                height = bmp.Height;
+            var result = new byte[3 * height * width];
+            var bd = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly,
+                PixelFormat.Format24bppRgb);
+            try
+            {
+                var lineSize = width * 3;
+                for (var h = 0; h < height; h++)
+                {
+                    var pos = h * lineSize;
+                    var curpos = (IntPtr)((byte*)bd.Scan0) + h * bd.Stride;
+                    Marshal.Copy(curpos, result, pos, lineSize);
+                }
+            }
+            finally
+            {
+                bmp.UnlockBits(bd);
+            }
+            return result;
+        }
+
+        public unsafe static void ByteToBitmapRgbMarshal(Bitmap bmp, byte[] result)
+        {
+            int width = bmp.Width,
+                height = bmp.Height;
+            //var result = new byte[3 * height * width];
+            var bd = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly,
+                PixelFormat.Format24bppRgb);
+            try
+            {
+                var lineSize = width * 3;
+                for (var h = 0; h < height; h++)
+                {
+                    var pos = h * lineSize;
+                    var curpos = (IntPtr)((byte*)bd.Scan0) + h * bd.Stride;
+                    Marshal.Copy(result, pos, curpos, lineSize);
+                }
+            }
+            finally
+            {
+                bmp.UnlockBits(bd);
+            }
+        }
+
+        /// <summary>
+        /// Метод загружает растровые изображения без блокирования файла
+        /// (как это делает конструктор Bitmap(fileName)).
+        /// </summary>
+        /// <param name="fileName">Имя файла для загрузки.</param>
+        /// <returns>Экземпляр Bitmap.</returns>
+        public static Bitmap LoadBitmap(string fileName)
+        {
+            using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                return new Bitmap(fs);
+            }
+        }
+    }
+}
---
+++ CandPCI_6/LsbMethod.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CandPCI_6
+{
+    public class LsbMethod
+    {
+        private byte[] data;
+        private int countLsBits;
+
+        public byte[] Data
+        {
+            get { return data; }
+        }
+
+        private int writePosition = 0;
+        private int readPosition = 0;
+
+        public LsbMethod(byte[] data, int countLsBits)
+        {
+            //this.data = data;
+            this.data = new byte[data.Length];
+
+            Array.Copy(data, this.data, data.Length);
+            if (countLsBits > 2 || countLsBits < 1)
+                throw  new ArgumentException();
+            this.countLsBits = countLsBits;
+        }
+
+        public void WriteBytes(byte[] bytes)
+        {
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                this[writePosition++] = bytes[i];
+            }
+        }
+
+        public void WriteInt(int value)
+        {
+            var bytes = BitConverter.GetBytes(value).Reverse().ToArray();
+            WriteBytes(bytes);
+        }
+
+        public void WriteLongInt(long value)
+        {
+            var bytes = BitConverter.GetBytes(value).Reverse().ToArray();
+            WriteBytes(bytes);
+        }
+
+        public void WriteString(string message)
+        {
+            var bytes = Encoding.Unicode.GetBytes(message);
+            WriteBytes(bytes);
+        }
+
+        public byte[] ReadBytes(int count)
+        {
+            var bytes = new byte[count];
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = this[readPosition++];
+            }
+            return bytes;
+        }
+
+        public long ReadLongInt()
+        {
+            return BitConverter.ToInt64(ReadBytes(8).Reverse().ToArray(), 0);
+        }
+
+        public int ReadInt()
+        {
+            return BitConverter.ToInt32(ReadBytes(4).Reverse().ToArray(), 0);
+        }
+
+        public string ReadMessage(int countBytes)
+        {
+            var bytes = ReadBytes(countBytes);
+            return Encoding.Unicode.GetString(bytes);
+        }
+
+        public byte this[int index]
+        {
+            get
+            {
+                var countBytesToRead = 8 / countLsBits;
+                var startIndex = index * countBytesToRead;
+                if (countBytesToRead + startIndex > data.Length)
+                    throw new IndexOutOfRangeException();
+                var result = 0;
+                var mask = 1 | countLsBits; // работает только с моим набором занчений
+                for (int i = 0; i < countBytesToRead; i++)
+                {
+                    result <<= countLsBits;
+                    result |= data[startIndex + i] & mask;
+                }
+                return (byte) result;
+            }
+
+            set
+            {
+                var countBytesToRead = 8 / countLsBits;
+                var startIndex = index * countBytesToRead;
+                if (countBytesToRead + startIndex > data.Length)
+                    throw new IndexOutOfRangeException();
+
+                var toWrite = value;
+                var mask = 128 | (countLsBits == 2 ? 64 : 0); // работает только с моим набором занчений
+                var clearMask = 255 - (1 | countLsBits); // работает только с моим набором занчений
+                var bitShift = 8 - countLsBits;
+                for (int i = 0; i < countBytesToRead; i++)
+                {
+                    data[startIndex + i] &= (byte)clearMask;
+                    data[startIndex + i] |= (byte)((toWrite & mask) >> bitShift);
+                    toWrite <<= countLsBits;
+                }
+            }
+        }
+    }
+}
