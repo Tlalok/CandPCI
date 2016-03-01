7f529dc23d1a2bef8a7504d1fa11b9a9c34474e5
Tlalok <irbis1307@yandex.ru>
2016-03-01 09:32:48 +0300

    add LSB UI

---
+++ CandPCI_6_UI/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace CandPCI_6_UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.addMessageButton = new System.Windows.Forms.Button();
+            this.AddingMessageBox = new System.Windows.Forms.TextBox();
+            this.AddedMessageBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(12, 12);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 51);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1056, 591);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // addMessageButton
+            // 
+            this.addMessageButton.Location = new System.Drawing.Point(608, 9);
+            this.addMessageButton.Name = "addMessageButton";
+            this.addMessageButton.Size = new System.Drawing.Size(92, 23);
+            this.addMessageButton.TabIndex = 2;
+            this.addMessageButton.Text = "Add Message";
+            this.addMessageButton.UseVisualStyleBackColor = true;
+            this.addMessageButton.Click += new System.EventHandler(this.addMessageButton_Click);
+            // 
+            // AddingMessageBox
+            // 
+            this.AddingMessageBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AddingMessageBox.Location = new System.Drawing.Point(368, 12);
+            this.AddingMessageBox.Name = "AddingMessageBox";
+            this.AddingMessageBox.Size = new System.Drawing.Size(234, 26);
+            this.AddingMessageBox.TabIndex = 3;
+            // 
+            // AddedMessageBox
+            // 
+            this.AddedMessageBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AddedMessageBox.Location = new System.Drawing.Point(834, 12);
+            this.AddedMessageBox.Name = "AddedMessageBox";
+            this.AddedMessageBox.ReadOnly = true;
+            this.AddedMessageBox.Size = new System.Drawing.Size(234, 26);
+            this.AddedMessageBox.TabIndex = 4;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(93, 12);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 5;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 654);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.AddedMessageBox);
+            this.Controls.Add(this.AddingMessageBox);
+            this.Controls.Add(this.addMessageButton);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.openButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button addMessageButton;
+        private System.Windows.Forms.TextBox AddingMessageBox;
+        private System.Windows.Forms.TextBox AddedMessageBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ CandPCI_6_UI/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CandPCI_6;
+using System.IO;
+
+namespace CandPCI_6_UI
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap picture;
+
+        private long prefix = 0x4BD94CD8BC3AA920;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            picture = BitmapHelper.LoadBitmap(openFileDialog.FileName);
+            //picture = new Bitmap(openFileDialog.FileName);
+            pictureBox.Image = picture;
+
+            var lsb = new LsbMethod(BitmapHelper.BitmapToByteRgbMarshal(picture), 1);
+            var readPrefix = lsb.ReadLongInt();
+            if (readPrefix != prefix)
+                return;
+            var readLengthMessage = lsb.ReadInt();
+            var readMessage = lsb.ReadMessage(readLengthMessage);
+            AddedMessageBox.Text = readMessage;
+        }
+
+        private void addMessageButton_Click(object sender, EventArgs e)
+        {
+            if (AddingMessageBox.Text.Length == 0)
+                return;
+            var bytes = BitmapHelper.BitmapToByteRgbMarshal(picture);
+
+            var lsb = new LsbMethod(bytes, 1);
+            lsb.WriteLongInt(prefix);
+            lsb.WriteInt(AddingMessageBox.Text.Length * 2);
+            lsb.WriteString(AddingMessageBox.Text);
+
+            //var mask = 256 - 1 - 2;
+            //for (var i = 0; i < bytes.Length; i++)
+            //    bytes[i] = (byte)(bytes[i] & mask);
+            BitmapHelper.ByteToBitmapRgbMarshal(picture, lsb.Data);
+            pictureBox.Image = picture;
+
+            lsb = new LsbMethod(BitmapHelper.BitmapToByteRgbMarshal(picture), 1);
+            var readPrefix = lsb.ReadLongInt();
+            var readLengthMessage = lsb.ReadInt();
+            var readMessage = lsb.ReadMessage(readLengthMessage);
+            MessageBox.Show((readPrefix == prefix).ToString());
+            AddedMessageBox.Text = readMessage;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            //using (var m = new MemoryStream())
+            //{
+            //    picture.Save(m, ImageFormat.Jpeg);
+
+            //    var img = Image.FromStream(m);
+
+            //    img.Save(saveFileDialog.FileName);
+            //}
+            //pictureBox.Image.Save(saveFileDialog.FileName, ImageFormat.Bmp);
+            var lsb = new LsbMethod(BitmapHelper.BitmapToByteRgbMarshal(picture), 1);
+            picture = new Bitmap(pictureBox.Image);
+            var readPrefix = lsb.ReadLongInt();
+            picture.Save(saveFileDialog.FileName, ImageFormat.Png);
+        }
+    }
+}
