1a1052005e2cc27f8b4d060818627f04fdc9cd9a
Vladislav Samodin <vladislav_samodin@epam.com>
2016-03-22 10:44:00 +0300

    delete unnecessary code

---
+++ CandPCI_3/Helpers/BigIntegerHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandPCI_3.Helpers
+{
+    public static class BigIntegerHelper
+    {
+        public static BigInteger PositiveOddRandom(BigInteger lowerBound, BigInteger upperBound)
+        {
+            byte[] bytes = upperBound.ToByteArray();
+            BigInteger result;
+            var random = new Random(DateTime.Now.Millisecond);
+            do
+            {
+                random.NextBytes(bytes);
+                bytes[bytes.Length - 1] &= (byte)0x7F; //force sign bit to positive
+                bytes[0] = (byte)(bytes[0] | 1);
+                result = new BigInteger(bytes);
+            } while (result < lowerBound || result >= upperBound);
+
+            return result;
+        }
+
+        public static BigInteger fast_exp(BigInteger a, BigInteger z, BigInteger n)
+        {
+            var a1 = a;
+            var z1 = z;
+            BigInteger x = 1;
+            while (z1 != 0)
+            {
+                while ((z1 % 2) == 0)
+                {
+                    z1 = z1 / 2;
+                    a1 = (a1 * a1) % n;
+                }
+                z1--;
+                x = (x * a1) % n;
+            }
+            return x;
+        }
+    }
+}
---
+++ CandPCI_3/PublicKey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandPCI_3
+{
+    [Serializable]
+    public class PublicKey
+    {
+        public BigInteger b { get; set; }
+        public BigInteger n { get; set; }
+    }
+}
