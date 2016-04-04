547f6c3607b81569d695c9b04ab03bf9a20a02bc
Tlalok <irbis1307@yandex.ru>
2016-04-04 19:32:37 +0300

    add project lab 4

---
+++ CandPCI_4/PrivateKey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandPCI_4
+{
+    class PrivateKey
+    {
+        BigInteger r { get; set; }
+        BigInteger d { get; set; }
+    }
+}
---
+++ CandPCI_4/PublicKey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandPCI_4
+{
+    class PublicKey
+    {
+        BigInteger r { get; set; }
+        BigInteger e { get; set; }
+    }
+}
